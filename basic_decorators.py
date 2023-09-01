31bdaae6e92299343d9f7617fb525fa7d780b136
valteem <val.teem.scn@gmail.com>
2023-09-01 18:38:47 +0300

    fixed basic decorator example
    added return statement inside wrapper
    
    default values for keyword arguments work just fine

--- basic_decorators.py
+++ basic_decorators.py
@@ -1,11 +1,8 @@
-# TODO: add support for default values of keyword arguments
-# https://stackoverflow.com/questions/31728346/passing-default-arguments-to-a-decorator-in-python
-
 def announce(text):
     def func_wrapper(f):
         def wrapper(*args, **kwargs):
             print(text)
-            f(*args, **kwargs)
+            return f(*args, **kwargs)
         return wrapper
     return func_wrapper
 
@@ -13,12 +10,9 @@ def announce(text):
 def multiply(a = 1, b = 1):
     return a * b
 
-def multiply_plain(a = 1, b = 1):
-    return a * b
-
 def main():
-    print(multiply())       # returns None
-    print(multiply_plain()) # returns 1
+    print(multiply())
+    print(multiply(2, 4))
 
 if __name__ == "__main__":
     main()
