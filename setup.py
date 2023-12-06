557f9e4fda86ab783f7e1b13d5305aad86c01c8d
valteem <val.teem.scn@gmail.com>
2023-12-06 09:32:48 +0300

    reorder project layout

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='langpy',
+    version='1.0.0',
+    url='https://github.com/py-lang.git',
+    author='Author Name',
+    author_email='author@mail.ext',
+    description='Basic Python language features',
+    packages=find_packages(),    
+)
