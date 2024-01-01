79ec693dec976bf917b1e28beb7ea4716bcd5012
valteem <val.teem.scn@gmail.com>
2024-01-01 15:10:55 +0300

    testing basic deque operations

---
+++ tests/test_deque_ops.py
@@ -0,0 +1,25 @@
+import pytest
+
+from collections import deque
+
+def test_basic():
+
+    d = deque([1, 2, 3, 4, 5])
+
+    """
+    negative offset - moving items left
+    """
+    d.rotate(-2)
+    assert [*d] == [3, 4, 5, 1, 2] #/ [*d] - unpack deque to list
+    """
+    positive offset - moving items right
+    """
+    d.rotate(4)
+    assert [*d] == [4, 5, 1, 2, 3]
+
+    d.extendleft([11, 12])
+    """extendleft() adds items from a list one by one, left to right, thus order of new items in resulting deque is reversed"""
+    assert [*d] == [12, 11, 4, 5, 1, 2, 3]
+
+    d.extend([21, 22])
+    assert [*d] == [12, 11, 4, 5, 1, 2, 3, 21, 22]
