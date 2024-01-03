86a3ce18142bfa6f3a238d718a6406ae32a7f3e1
valteem <val.teem.scn@gmail.com>
2024-01-03 09:02:02 +0300

    move test_simple.py to tests folder

--- langpy/test_simple.py
+++
@@ -1,5 +0,0 @@
-import simple
-
-def test_add():
-    assert simple.add(1, 1) == 2
-    assert simple.add(2, 2) == 5
---
+++ tests/test_simple.py
@@ -0,0 +1,5 @@
+from langpy.simple import add
+
+def test_add():
+    assert add(1, 1) == 2
+    assert add(2, 2) == 5
