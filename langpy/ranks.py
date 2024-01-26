653b7303fbb4367beb2e47b3aa739e0fa3e26f3d
valteem <val.teem.scn@gmail.com>
2024-01-26 15:02:18 +0300

    convert numeric string with whitespaces to list of integers

--- langpy/ranks.py
+++ langpy/ranks.py
@@ -11,3 +11,13 @@ def round_grades(grades):
                 y = x
         rounded.append(y)
     return rounded
+
+def convert_str_to_list_of_int(s: str) -> list[int]:
+    """
+    https://stackoverflow.com/a/8270124
+    To remove all whitespace characters (space, tab, newline, and so on) you can use split() then join()
+    """
+    if ''.join(s.split()).isnumeric():
+        return list(map(int, s.rstrip().split()))
+    else:
+        return []
--- tests/test_ranks.py
+++ tests/test_ranks.py
@@ -1,9 +1,14 @@
 import pytest
 
 from langpy.ranks import round_grades
+from langpy.ranks import convert_str_to_list_of_int
 
 def test_round_grades():
 
     test_grades = [73, 67, 38, 33]
     rounded_grades = round_grades(test_grades)
     assert rounded_grades == [75, 67, 40, 33]
+
+def test_convert_str_to_list_of_int():
+    assert convert_str_to_list_of_int("11 21 31 41\n") == [11, 21, 31, 41]
+    assert convert_str_to_list_of_int("11 21 31 41 a\n") == []
