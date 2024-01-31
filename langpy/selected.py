c4ef2fd49ac34205b088cf1292fd11a8c0d84f5b
valteem <val.teem.scn@gmail.com>
2024-01-31 12:34:55 +0300

    IntEnum example

--- langpy/selected.py
+++ langpy/selected.py
@@ -4,3 +4,18 @@ An assortment of utility functions from across various repositories
 
 def name_encode(name:str, pos: int) -> bytes:
     return name.encode('ascii', errors='replace')[:pos]
+
+from enum import IntEnum
+
+"""
+New in 3.11:
+@verify(UNIQUE)
+"""
+class EnumVal(IntEnum):
+    SEL_1 = 1
+    SEL_2 = 3
+    SEL_3 = 2
+
+SEL_1 = EnumVal.SEL_1
+SEL_2 = EnumVal.SEL_2
+SEL_3 = EnumVal.SEL_3
--- tests/test_selected.py
+++ tests/test_selected.py
@@ -1,6 +1,21 @@
 import pytest
 
+from typing import TYPE_CHECKING
+
 from langpy.selected import name_encode
+
+def test_type_checking():
+    if TYPE_CHECKING:
+        assert 1 == 2
+    else:
+        assert 1 == 1
 
 def test_name_encode():
     assert name_encode('0123456789abcdefgh', 8) == b"01234567"
+
+from langpy.selected import EnumVal
+
+def test_enum_val():
+    assert EnumVal.SEL_1 == 1
+    assert EnumVal.SEL_2 == 3
+    assert EnumVal.SEL_3 == 2
