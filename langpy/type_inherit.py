51e3f48b8aee0aa907c2a564375895f8bf09641a
valteem <val.teem.scn@gmail.com>
2025-05-26 11:19:30 +0300

    type_inherit: fix class attribute definition

--- langpy/type_inherit.py
+++ langpy/type_inherit.py
@@ -1,6 +1,6 @@
 class A:
     def __init__(self, v):
-        A.value = v
+        self.value = v
     def __eq__(self, other) -> bool:
         return isinstance(other, A) and self.value is other.value
 
