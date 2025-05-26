2be4b92251817714ce9929774556a02479b559e1
valteem <val.teem.scn@gmail.com>
2025-05-26 12:29:11 +0300

    nested_func: adjust return type annotation: use typing.Callable

--- langpy/nested_func.py
+++ langpy/nested_func.py
@@ -1,4 +1,6 @@
-def outer(a: int, b: int) -> callable:
+from typing import Callable
+
+def outer(a: int, b: int) -> Callable:
     def inner(x: int, y: int) -> int:
         return (a + b) * x + y
     return inner
