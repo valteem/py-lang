d6690cb20ac84c7c3c5aff58f8c484c64ad560a2
valteem <val.teem.scn@gmail.com>
2025-05-26 12:34:06 +0300

    hashopt: user tuple[] in return type annotation

--- langpy/hashopt.py
+++ langpy/hashopt.py
@@ -4,5 +4,5 @@ Prep stuff for various hashing options
 Loosely based on CLRS 11.3 examples
 """
 
-def div_mod_power_of_2(input: int, mod_power_of_2: int) -> (int, int):
+def div_mod_power_of_2(input: int, mod_power_of_2: int) -> tuple[int, int]:
     return (input % (1 << mod_power_of_2), input & ((1 << mod_power_of_2) - 1)) # h(k) is just the p lowest-order bits of k
