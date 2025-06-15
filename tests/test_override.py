4a0ef1d15bea3ac087aec12740590bfed14b9aa3
valteem <val.teem.scn@gmail.com>
2025-06-15 15:51:14 +0300

    tweak @overrride example

--- tests/test_override.py
+++ tests/test_override.py
@@ -4,11 +4,10 @@ from langpy.override import Base, Heir
 
 def test_override():
 
-    b = Base()
-    h = Heir(2, "Heir")
+    b = Base(1)
+    h1 = Heir("apples")
+    h2 = Heir(2)
 
-    assert b.show() == (1, "Base")
-    assert h.show() == "2 Heir"
-
-    assert b.add() == 5
-    assert h.add() == 6
+    assert b.show() == 1
+    assert h1.show() == "apples"
+    assert h2.show() == "2"
